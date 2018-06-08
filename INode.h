624223a41f6d54f3dce1e2dd155bfd9488adbbd8
root <root@nur1abs.nur1labshq.net>
2018-06-08 18:27:39 +0900

    nodes fixed parent block

--- INode.h
+++ INode.h
@@ -78,7 +78,7 @@ public:
   virtual uint32_t getLocalBlockCount() const = 0;
   virtual uint32_t getKnownBlockCount() const = 0;
   virtual uint64_t getLastLocalBlockTimestamp() const = 0;
-  //virtual BlockHeaderInfo getLastLocalBlockHeaderInfo() const = 0;
+  virtual BlockHeaderInfo getLastLocalBlockHeaderInfo() const = 0;
 
   virtual void relayTransaction(const Transaction& transaction, const Callback& callback) = 0;
   virtual void getRandomOutsByAmounts(std::vector<uint64_t>&& amounts, uint64_t outsCount, std::vector<CryptoNote::COMMAND_RPC_GET_RANDOM_OUTPUTS_FOR_AMOUNTS::outs_for_amount>& result, const Callback& callback) = 0;
